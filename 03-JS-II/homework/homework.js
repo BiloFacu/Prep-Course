8b3afc29bf74d73d564f4ae4a1b8c1620e88733e
BiloFacu <bilofacundo@gmail.com>
2022-08-09 04:07:40 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -128,16 +128,16 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if(num1 > num2 && num1 > num3 && num1 > 0){
-    return "Número 1 es mayor y positivo";
-  } else if(num1 < 0 || num2 < 0 || num3 < 0){
-    return "Hay negativos"
-  } else if(num3 > num1 && num3 > num2){
-    return num3+1;
+  if(num1 < 0 || num2 < 0 || num3 < 0){
+    return "Hay negativos";
+  } else if(num1 > num2 && num1 > num3 && num1 > 0){
+    return "Número 1 es mayor y positivo"
   } else if(num1 === 0 || num2 === 0 || num3 === 0){
     return "Error"
+  } else if(num3 > num1 && num3 > num2){
+    return num3+1;
   } else {
-    return false
+    return false;
   }
 }
 
@@ -147,11 +147,18 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if(numero % numero === 0 && numero > 1){
-    return true;
-  } else {
+  if (numero < 2){
     return false;
   }
+  if (numero === 2){
+    return true;
+  }
+  for(var i = 2; i < numero; i++){
+    if(numero % i === 0){
+    return false;
+  }
+}
+return true;
 }
 
 function esVerdadero(valor){
@@ -169,11 +176,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  let arrayTablaDel16 = [0]
-  for(i === 0; i === 60; i + 6){
-    arrayTablaDel16.push(i)
+  let arrayTablaDel6 = [];
+  for(let i = 0; i < 11; i ++){
+    arrayTablaDel6.push(6 * i)
   }
-  return arrayTablaDel16;
+return arrayTablaDel6;
 }
 
 function tieneTresDigitos(numero){
